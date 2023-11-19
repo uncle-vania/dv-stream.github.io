0daef25c06c087e6fdd1c41b9b084056fe226af2
uncle-vania <uncle-vania@users.noreply.github.com>
2023-11-19 11:22:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 swfobject.embedSWF("img/christmastree.swf", "tree", "104", "152", "9.0.0", null, null, {wmode:"transparent"});
 
     //VK.Widgets.Comments("vk_comments", {limit: 30, attach: "*", autoPublish: 0});
-    //VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*"});
+    VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*"}, 5192617);
 //VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*", pageUrl: "uncle-vania.ga"});
-    VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*", pageUrl: "dv-stream.ru"});
+    //VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*", pageUrl: "dv-stream.ru"});
 
 $('#donate').on('show.bs.modal', function () {
     $('html, body').animate({
